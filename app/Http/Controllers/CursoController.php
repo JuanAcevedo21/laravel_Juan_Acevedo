cfce0a03fbb8b3e7d0ac7c507a8dec3e88682d06
JuanAcevedo21 <juandavid123ace@gmail.com>
2024-05-15 16:29:38 -0500

    se agrega el proyecto

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Curso;
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('cursos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //return $request->all(); Con esto se devuelve la informacion que se ingresa
+        $course = new Curso();
+        $course->nombre = $request->input('nombre');
+        $course->descripcion = $request->input('descripcion');
+
+        $course->save();
+        return 'Guardado exitoso';
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
